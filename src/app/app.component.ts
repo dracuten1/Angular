7f99956fb8b3c2250e40b2d0842e06d928d67851
tuyen <tuyen.nguyen@yedda.io>
2020-10-08 10:26:42 +0700

    add jenkinsfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient){
+    this.http.get("http://api-cashier-mail-browser.yedda.tech/companies/by-id/941/files?limit=20&time_from=2019-06-23T08:10:55.000Z&time_to=2021-09-22T08:14:38.418Z").subscribe(data=>{
+      console.log(data);
+    })
+  }
   title = 'angular';
 }
